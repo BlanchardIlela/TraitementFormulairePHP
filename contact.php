106fc9bdf169421740d55900b585e3000887b3a6
BlanchardIlela <blancilela@gmail.com>
2026-02-03 06:44:00 +0100

    Page de contact

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php 
+$title = "Nous contacter";
+require 'header.php'; 
+?>
+
+<h2>Nous contacter</h2>
+
+<p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quibusdam in dolorum. Distinctio cumque nihil nulla excepturi rerum, nobis, porro, praesentium quas fuga sapiente explicabo fugiat? Ipsa tempora nostrum nam.
+</p>
+
+<?php require 'footer.php'; ?>
