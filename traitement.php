12b5224823dc6caa3620deafe0008197a68faed6
BlanchardIlela <blancilela@gmail.com>
2026-02-03 13:04:11 +0100

    Modification de la boucle pour calculer les parfums et supplements

--- traitement.php
+++ traitement.php
@@ -19,22 +19,16 @@ $supplements = [
 $title = "Composer votre glace";
 $ingredients = [];
 $total = 0;
-if (isset($_GET['parfum'])) {
-    foreach ($_GET['parfum'] as $parfum) {
-        if(isset($parfums[$parfum])) {
-            $ingredients[] = $parfum;
-            $total += $parfums[$parfum];
+foreach (['parfum', 'supplement'] as $name) {
+    if (isset($_GET[$name])) {
+        $liste = $name . 's';
+    foreach ($_GET[$name] as $value) {
+        if(isset($$liste[$value])) {
+            $ingredients[] = $value;
+            $total += $$liste[$value];
         }
     }
-}
-
-if (isset($_GET['supplement'])) {
-    foreach ($_GET['supplement'] as $supplement) {
-        if(isset($supplements[$supplement])) {
-            $ingredients[] = $supplement;
-            $total += $supplements[$supplement];
-        }
-    }
+   }
 }
 
 if (isset($_GET['cornet'])) {
