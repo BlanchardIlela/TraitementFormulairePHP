6670ab7bc405925b247d472571f494e35398fe21
BlanchardIlela <blancilela@gmail.com>
2026-02-06 04:39:43 +0100

    Création de fonction creneaux_html

--- functions.php
+++ functions.php
@@ -50,4 +50,12 @@ HTML;
         echo '</pre>';
     }
 
+    function creneaux_html (array $creneaux) {
+        $phrases = [];
+        foreach ($creneaux as $creneau) {
+            $phrases[] = "de <strong>{$creneau[0]}h</strong> à <strong>{$creneau[1]}h</strong>";
+        }
+        return 'Ouvert ' . implode(' et ', $phrases);
+    }
+
 ?>
