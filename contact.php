4c9d2fc6ea8937db7d1bda31932cd22252f98a7d
BlanchardIlela <blancilela@gmail.com>
2026-02-06 04:41:12 +0100

    Affichage de l'horaire

--- contact.php
+++ contact.php
@@ -1,12 +1,24 @@
 <?php 
 $title = "Nous contacter";
+require_once 'config.php';
+require_once 'functions.php';
+$creneaux = creneaux_html(CRENEAUX);
 require 'header.php'; 
 ?>
 
-<h2>Nous contacter</h2>
+<div class="row">
+    <div class="col-md-8">
+        <h2>Nous contacter</h2>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quibusdam in dolorum. Distinctio cumque nihil nulla excepturi rerum, nobis, porro, praesentium quas fuga sapiente explicabo fugiat? Ipsa tempora nostrum nam.
+        </p>
+    </div>
+    <div class="col-md-4">
+        <h2>Horaire d'ouverture</h2>
+        <?= $creneaux ?>
+    </div>
+</div>
 
-<p>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quibusdam in dolorum. Distinctio cumque nihil nulla excepturi rerum, nobis, porro, praesentium quas fuga sapiente explicabo fugiat? Ipsa tempora nostrum nam.
-</p>
+
 
 <?php require 'footer.php'; ?>
