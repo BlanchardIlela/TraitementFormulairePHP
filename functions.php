f9248a8ab5fac3eeba72a4bfac3336590c7828ab
BlanchardIlela <blancilela@gmail.com>
2026-02-06 23:38:42 +0100

    Mise en place fonction select

--- functions.php
+++ functions.php
@@ -43,6 +43,15 @@ HTML;
 HTML;
     }
 
+    function select(string $name, $value, array $options): string {
+        $html_options = [];
+        foreach ($options as $k => $option) {
+           $attributes = $k == $value ? 'selected' : '';
+           $html_options[] = "<option value='$k' $attributes>$option</option>";
+        }
+        return "<select class='form-control' name='$name'>" . implode($html_options) . '</select>';
+    }
+
     function dump ($variable)
     {
         echo '<pre>';
