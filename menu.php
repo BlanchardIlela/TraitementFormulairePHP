b23942e1adb6e2c69144712a05776dd68f6966fd
BlanchardIlela <blancilela@gmail.com>
2026-02-12 02:16:24 +0100

    Récupération dans tableau de chaque lignes avec la fonction php explode

--- menu.php
+++ menu.php
@@ -1,9 +1,12 @@
 <?php
 $title = "Notre menu";
 $menu = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'menu.tsv');
+$lignes = explode(PHP_EOL, $menu);
 require 'elements/header.php';
 ?>
 
 <h1>Menu</h1>
+
+<?= dump($lignes) ?>
 
 <?php require 'elements/footer.php'; ?>
