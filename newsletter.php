53940bfc87840e755b29e77f2ae6fe0f9c457b62
BlanchardIlela <blancilela@gmail.com>
2026-02-12 03:01:35 +0100

    Mise du formulaire

--- newsletter.php
+++ newsletter.php
@@ -2,4 +2,17 @@
 require 'elements/header.php';
 ?>
 
+<h1>S'inscrire à la newsletter</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia fugiat voluptates dolor minima est, recusandae repellendus vero quod? Eaque aspernatur quae cupiditate velit minima quod quam blanditiis quo incidunt placeat?
+</p>
+
+<form action="/newsletter.php" method="post" class="form-inline">
+    <div class="form-group">
+        <input type="email" name="email" id="" placeholder="Entrer votre email" required class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">S'inscrire</button>
+</form>
+
 <?php require 'elements/footer.php'; ?>
