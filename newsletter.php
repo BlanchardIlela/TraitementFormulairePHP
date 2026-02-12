c9455b6b17ad8b9a699018140c86218cd3f662bd
BlanchardIlela <blancilela@gmail.com>
2026-02-12 03:22:36 +0100

    Affichage de message de success

--- newsletter.php
+++ newsletter.php
@@ -1,10 +1,11 @@
 <?php 
 $error = null;
+$success = null;
 $email = null;
 if (!empty($_POST['email'])) {
     $email = $_POST['email'];
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        
+        $success = "Votre email a bien été enregistré";
     } else {
         $error = "Email invalide";
     }
@@ -23,6 +24,12 @@ require 'elements/header.php';
 <?php if($error): ?>
     <div class="alert alert-danger">
         <?= $error ?>
+    </div>
+<?php endif ?>
+
+<?php if($success): ?>
+    <div class="alert alert-success">
+        <?= $success ?>
     </div>
 <?php endif ?>
 
