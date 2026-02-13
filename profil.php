bae1ab47e8a7298cc5931f32697ffabc62ff063d
BlanchardIlela <blancilela@gmail.com>
2026-02-13 02:26:15 +0100

    Mise en place d'une petit formulaire

--- profil.php
+++ profil.php
@@ -2,4 +2,11 @@
 require 'elements/header.php';
 ?>
 
+<form action="" method="post">
+    <div class="form-group">
+        <input class="form-control" type="text" name="nom" id="" placeholder="Entrer votre nom">
+    </div>
+    <button class="btn btn-primary" type="submit">Se connecter</button>
+</form>
+
 <?php require 'elements/footer.php'; ?>
