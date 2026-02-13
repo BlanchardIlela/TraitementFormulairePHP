4bd9cfad0bd955382ab8ffca6bfd813db291b1ca
BlanchardIlela <blancilela@gmail.com>
2026-02-13 14:44:31 +0100

    Affichage de vues via la variable

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,21 @@
 <?php 
+require 'functions/compteur.php';
+$total = nombre_vues();
 require 'elements/header.php';
 ?>
 
-<div class="card">
-    Visite total
+<div class="row">
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-body">
+                <strong class="font-size:3em"><?= $total ?></strong><br>
+                Visite<?= $total > 1 ? 's': '' ?> total
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php require 'elements/footer.php'; ?>
