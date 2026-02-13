882994c4617fd09923419ea9bbf7e01a7c845842
BlanchardIlela <blancilela@gmail.com>
2026-02-13 02:34:36 +0100

    La définition de cookie avec comme nom utilisateur ensuite nous affichons le nom entre avec une salutation de la personne.

--- profil.php
+++ profil.php
@@ -1,12 +1,24 @@
 <?php
+$nom = null;
+if (!empty($_COOKIE['utilisateur'])) {
+    $nom = $_COOKIE['utilisateur'];
+}
+if (!empty($_POST['nom'])) {
+    setcookie('utilisateur', $_POST['nom']);
+    $nom = $_POST['nom'];
+}
 require 'elements/header.php';
 ?>
 
-<form action="" method="post">
-    <div class="form-group">
-        <input class="form-control" type="text" name="nom" id="" placeholder="Entrer votre nom">
-    </div>
-    <button class="btn btn-primary" type="submit">Se connecter</button>
-</form>
+<?php if($nom): ?>
+    <h1>Bonjour <?= htmlentities($nom) ?></h1>
+<?php else: ?>
+    <form action="" method="post">
+        <div class="form-group">
+            <input class="form-control" type="text" name="nom" id="" placeholder="Entrer votre nom">
+        </div>
+        <button class="btn btn-primary" type="submit">Se connecter</button>
+    </form>
+<?php endif ?>
 
 <?php require 'elements/footer.php'; ?>
