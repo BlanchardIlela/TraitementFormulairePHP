7fa70c0a31aad0eb3655011fece1b4c852d7244d
BlanchardIlela <blancilela@gmail.com>
2026-02-16 06:31:51 +0100

    Ajout de la fonction forcer_utilisateur_connecte()

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,7 @@
 <?php 
-require 'functions/compteur.php';
+require_once 'functions/auth.php';
+forcer_utilisateur_connecte();
+require_once 'functions/compteur.php';
 $annee = (int)date('Y');
 $annee_selection = empty($_GET['annee']) ? null : (int)$_GET['annee'];
 $mois_selection = empty($_GET['mois']) ? null : $_GET['mois'];
@@ -24,7 +26,7 @@ $mois = [
     '12' => 'Décembre'
 
 ];
-require 'elements/header.php';
+require_once 'elements/header.php';
 ?>
 
 <div class="row">
